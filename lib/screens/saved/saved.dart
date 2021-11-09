3074f3bde20469eb5c2c500ce6b1d15f198aec16
sharonhsl <hslam2@illinois.edu>
2021-11-08 19:55:35 -0600

    merge conflict

--- lib/screens/saved/saved.dart
+++ lib/screens/saved/saved.dart
@@ -1,44 +1,85 @@
 import 'package:flutter/material.dart';
-import 'package:mappu/models/saved_article.dart';
-import '../../db/database_helper.dart';
+import 'package:mappu/models/news_article.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
- class SavedWidget extends StatefulWidget {
+class SavedWidget extends StatelessWidget {
   const SavedWidget({Key? key}) : super(key: key);
+  static const List<Tab> tabs = <Tab>[
+    Tab(text: 'AF'),
+    Tab(text: 'AN'),
+    Tab(text: 'AS'),
+    Tab(text: 'EU'),
+    Tab(text: 'NA'),
+    Tab(text: 'OC'),
+    Tab(text: 'SA'),
+  ];
 
-  @override
-  State<SavedWidget> createState() => _SavedWidgetState();
-}
+  Widget _buildList() {
+    return ListView(
+      children: [
+        _tile(NewsArticle(title: "Safety meeting ends in accident",
+            link: "http://google.com/",
+            pubDate: DateTime(2021,11,03),
+            source: "The Cat",
+            sourceURL: "google.com")),
+        _tile(NewsArticle(title: "World Bank says poor need more money",
+            link: "http://google.com/",
+            pubDate: DateTime(2021,10,21),
+            source: "The Cat",
+            sourceURL: "google.com")),
+        _tile(NewsArticle(title: "Murderer says detective ruined his reputation",
+            link: "http://google.com/",
+            pubDate: DateTime(2021,10,15),
+            source: "The Cat",
+            sourceURL: "google.com")),
+        _tile(NewsArticle(title: "Man gets 30 years then slaps attorney",
+            link: "http://google.com/",
+            pubDate: DateTime(2021,10,1),
+            source: "The Cat",
+            sourceURL: "google.com")),
+      ],
+    );
+  }
 
-class _SavedWidgetState extends State<SavedWidget> {
-  late List<SavedArticle> savedArticles;
+  ListTile _tile(NewsArticle article) {
+    return ListTile(
+      title: Text(article.title,
+          style: const TextStyle(
+            fontWeight: FontWeight.w500,
+            fontSize: 20,
+          )),
+      subtitle: Text(timeago.format(article.pubDate),
+          style: TextStyle(
+            fontSize: 10.0,
+            color: Colors.grey[500],
+          )),
+      leading: Icon(Icons.emoji_flags)
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
-    );
-  }
-
-  Future refreshSavedArticles() async {
-    SavedArticle savedArticle = SavedArticle(articleId: "hello",
-      title: 'article title',
-      link: 'article link',
-      pubDate: DateTime.now(),
-      source: 'bbc',
-      sourceURL: 'bbc.com',
-      countryId: 'JP',
-      savedAt: DateTime.now(),
-    );
-    await DatabaseHelper.instance.insertSavedArticle(savedArticle);
-
-    savedArticles = await DatabaseHelper.instance.getSavedArticles();
-    print(savedArticles);
-  }
-
-  @override
-  void initState() {
-    super.initState();
-
-    refreshSavedArticles();
+    return DefaultTabController(
+        length: tabs.length,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: const TabBar(
+              tabs: tabs
+            ),
+            title: const Text('Your Saved Articles'),
+          ),
+          body: TabBarView(
+            children: [
+              _buildList(),
+              _buildList(),
+              _buildList(),
+              _buildList(),
+              _buildList(),
+              _buildList(),
+              _buildList(),
+            ],
+          ),
+        ),
+      );
   }
 }
