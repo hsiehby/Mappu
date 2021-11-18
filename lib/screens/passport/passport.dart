cd18418fc64fddc0d3af9129a5900634d072ceb7
謝秉元 <hsiehbj@xiebingjunde-MacBook-Pro.local>
2021-11-18 00:29:33 -0600

    Removed currently unused code.

--- lib/screens/passport/passport.dart
+++ lib/screens/passport/passport.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:mappu/screens/passport/components/level_bar.dart';
-import 'package:mappu/screens/passport/components/user_statistics.dart';
 import 'components/stamps_preview.dart';
 import 'components/profile.dart';
 import 'components/stats.dart';
@@ -14,23 +12,11 @@ class PassportWidget extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: const [
-            // Padding(
-            //   padding: EdgeInsets.fromLTRB(24.0, 8.0, 24.0, 8.0),
-            //   child: Align(
-            //     alignment: Alignment.topLeft,
-            //     child: Text(
-            //       "Hello!",
-            //       style: TextStyle(fontSize: 56.0, fontWeight: FontWeight.w600)
-            //     ),
-            //   ),
-            // ),
             SizedBox(height: 36.0),
             Profile(),
             SizedBox(height: 12.0),
             Stats(),
             StampsPreview(),
-            // LevelBar(),
-            // UserStatistics()
           ],
         ),
       )
